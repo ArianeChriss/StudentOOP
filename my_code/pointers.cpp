266a86f1810b3a81f08e0cfc28b600341d4e2ce5
ArianeChriss <45075120+ArianeChriss@users.noreply.github.com>
2019-06-04 12:16:17 -0400

    pointer test

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -4,28 +4,44 @@
 using namespace std;
 
 void inc_by_value(int n) {
+	++n;
 }
 
 
 void inc_with_pointer(int* iptr) {
+	++(*iptr);
 }
 
 
 void inc_with_reference(int& n) {
+	++n;
 }
 
 
 Thing** create_array_of_things(int n) {
-    return nullptr;
+	Thing** things = new Thing*[n];
+	for (int i = 0; i < n; ++i) {
+		*(things + i) = new Thing(i);
+	}
+	return things;
 }
 
 void print_all_things(Thing** things, int n) {
+	for (int i = 0; i < n; ++i) {
+		cout << (*(things + i))->val << endl;
+	}
 }
 
 void double_all_things(Thing** things, int n) {
+	for (int i = 0; i < n; ++i) {
+		(*(things + i))->val = ((*(things + i))->val) * 2;
+	}
 }
 
 void delete_all_things(Thing** things, int n) {
+	for (int i = n - 1; i >= 0; --i) {
+		*(things + i) = nullptr;
+	}
 }
 
 
@@ -34,4 +50,3 @@ void assignTA(Student& s, Student& ta) {
 
 void printTAs(vector<Student> students) {
 }
-
