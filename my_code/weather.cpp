8542d55dea0190440a0f17524bc62d3b38b154a5
ArianeChriss <45075120+ArianeChriss@users.noreply.github.com>
2019-06-12 13:02:56 -0400

    copy control

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -4,6 +4,56 @@
 
 using namespace std;
 
+const double F_TO_C = 5 / 9;
+const double C_TO_F = 9 / 5;
+
+Image::Image(int w, int h, std::string flnm)
+	: width(w), height(h)
+{
+	filename = flnm;
+	image_buf = new char[image_sz()];
+}
+
+// copy constructor:
+Image::Image(const Image& img2) {
+	Image temp = Image(img2.width, img2.height, img2.filename);
+	temp.image_buf = new char[img2.width * img2.height];
+	temp.copy_fields(img2);
+}
+
+Image::~Image() {
+	if (image_buf != nullptr) delete image_buf;
+}
+
+Image& Image::operator=(const Image& img2) {
+	copy_fields(img2);
+	return *this;
+}
+
+int Image::image_sz() {
+	return width * height;
+}
+
+
+void Image::copy_fields(const Image& img2) {
+	width = img2.width;
+	height = img2.height;
+	filename = img2.filename;
+	for (int i = 0; i < image_sz(); ++i) {
+		image_buf[i] = img2.image_buf[i];
+	}
+}
+
+
+/*
+ * Setting `display() = 0` here makes this an abstract
+ * class that can't be implemented.
+ * */
+string Image::display(std::string s) {
+	return "Displaying image " + s;
+}
+
+
 /*
  * A constructor for weather class.
  * */
@@ -25,6 +75,10 @@ int Weather::get_rating() const {
 
 void Weather::set_rating(int new_rating) {
 	rating = new_rating;
+}
+
+double WReading::get_tempF() {
+	return (temperature * C_TO_F) + 32;
 }
 
 /*double Weather::get_heat_index(WReading& wr) { // need temperature and humidity
--- my_code/weather.h
+++ my_code/weather.h
@@ -5,6 +5,40 @@
 #include <vector>
 #include "date.h"
 
+/*
+ * This will be the base class for all types of images.
+ * */
+class Image {
+public:
+	Image(int w, int h, std::string flnm);
+	// copy constructor:
+	Image(const Image& img2);
+	~Image();
+	Image& operator=(const Image& img2);
+	int image_sz();
+
+	/*
+	 * Setting `display() = 0` here makes this an abstract
+	 * class that can't be implemented.
+	 * */
+	std::string display(std::string s);
+	/*
+	 * If we don't want virtual method lookup, we
+	 * could just declare:
+	 * void display();
+	 * */
+
+	int get_height() { return height; }
+	int get_width() { return width; }
+
+private:
+	int width;
+	int height;
+	std::string filename;
+	char* image_buf;
+	void copy_fields(const Image& img2);
+};
+
 struct GPS {
     double latitude;
     double longitude;
@@ -22,6 +56,9 @@ class WReading {
 	int get_temp() { return temperature; }
 	int get_humidity() { return humidity; }
 	int get_windspeed() { return windspeed; }
+	double get_tempF();
+	double get_tempC() { return temperature; }
+
  private:
     Date date;
     double temperature;  // stored temp in C
@@ -44,6 +81,7 @@ class Weather {
     int get_rating() const;
     void set_rating(int new_rating);
     void add_reading(WReading wr);
+
 	// double get_heat_index(WReading& wr);
 	// double get_wind_chill(WReading& wr);
  private:
